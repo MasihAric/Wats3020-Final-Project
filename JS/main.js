fdfb7287e0bea1c747f183a26af434424fd790bb
Aric Masih <masiharic@seattleu.edu>
2019-03-14 18:18:16 -0700

    Added color.

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+//Make use of both Array and Object data structures.
+//Make use of at least TWO conditional statements that play a significant role in your code.
+//Make use of either Functions or Classes to organize your code.
+//Make use of at least TWO Event listeners and handlers to respond to user interaction.
+//Perform at least TWO string modifications/replacements on the page.
+
