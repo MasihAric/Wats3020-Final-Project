a7b1e52087978464c37e0ad24db058e55b1bee71
Aric Masih <masiharic@seattleu.edu>
2019-03-20 06:54:37 -0700

    updates

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,11 @@
 //Make use of either Functions or Classes to organize your code.
 //Make use of at least TWO Event listeners and handlers to respond to user interaction.
 //Perform at least TWO string modifications/replacements on the page.
+let person = {
+    gendertype = "male", "female";
+}
+
+funtion myFunction() {
+    var x = document.getElementById("gender").value;
+    document.getElementById("result").innerHTML = x;
+}
