c2e0f4675202bba18d9cc1a3f5347169ffbee37c
Aric Masih <masiharic@seattleu.edu>
2019-03-24 15:35:04 -0700

    new updates

--- JS/main.js
+++ JS/main.js
@@ -3,11 +3,22 @@
 //Make use of either Functions or Classes to organize your code.
 //Make use of at least TWO Event listeners and handlers to respond to user interaction.
 //Perform at least TWO string modifications/replacements on the page.
-let person = {
-    gendertype = "male", "female";
+//document.addEventListener('DOMContentLoaded', function(g){
+//    docment.addEventListener('submit', function(g){
+//        g.preventDefault();
+//        let 
+//    })
+//});
+function Cal(){
+    let gender = document.getElementById("gender").nodeValue;
+    let age = document.getElementById("age").nodeValue;
+    let weight = document.getElementById("weight").nodeValue;
+    if(age == 0 || weight  == 0 || gender === "")
+    alert("Please enter values");
+    return;
+    if(age <= 18 || weight <= 125)
+    console.log("You should 100 grams of protein for the day!")
 }
-
-funtion myFunction() {
-    var x = document.getElementById("gender").value;
-    document.getElementById("result").innerHTML = x;
+docueent .getElementById("calculate").onclick = function(){
+    Cal();
 }
