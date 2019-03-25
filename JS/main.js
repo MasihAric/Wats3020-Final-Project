4652726f036b679f65243d04b0f40f5413a0f4a7
Aric Masih <masiharic@seattleu.edu>
2019-03-25 01:03:38 -0700

    got the button to display result

--- JS/main.js
+++ JS/main.js
@@ -3,21 +3,26 @@
 //Make use of either Functions or Classes to organize your code.
 //Make use of at least TWO Event listeners and handlers to respond to user interaction.
 //Perform at least TWO string modifications/replacements on the page.
-//document.addEventListener('DOMContentLoaded', function(g){
-//    docment.addEventListener('submit', function(g){
-//        g.preventDefault();
-//        let 
-//    })
-//});
+//document.addEventListener('DOMContentLoaded', function Cal(){
+//    document.addEventListener('submit', function Cal(){
+//    g.preventDefault();
+//})
 function Cal(){
-    let gender = document.getElementById("gender").nodeValue;
-    let age = document.getElementById("age").nodeValue;
-    let weight = document.getElementById("weight").nodeValue;
+    let gender = document.getElementById("gender").value;
+    let age1 = document.getElementById("age").value;
+    let weight1 = document.getElementById("weight").value;
     
-    if(age == 0 || weight == 0 || gender === "")
-    alert("Please enter values");
-    return;
-    document.getElementById("calculate").onclick = function(){
-    Cal();
+    let result = parseFloat(age1)+parseFloat(weight1);
+    if(!isNaN(result)){
+      document.getElementById("answer").innerHTML="You need this much protein in grams: "+ result;
     }
+    //if(age == 0 || weight == 0 || gender == "")
+    //alert("Please enter values");
+    
+    //if(gender == "Male" || age <= 18 || weight <=130){
+    //    console.log("200 grams");
+    //}
+    
+    //document.getElementById("calculate").onclick = value;
 }
+//})
