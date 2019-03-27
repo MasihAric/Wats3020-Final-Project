955e762bc1c2745c7eb2dbfa78a7a57278213d89
Aric Masih <masiharic@seattleu.edu>
2019-03-26 19:44:58 -0700

    added dom

--- JS/main.js
+++ JS/main.js
@@ -3,26 +3,40 @@
 //Make use of either Functions or Classes to organize your code.
 //Make use of at least TWO Event listeners and handlers to respond to user interaction.
 //Perform at least TWO string modifications/replacements on the page.
-//document.addEventListener('DOMContentLoaded', function Cal(){
-//    document.addEventListener('submit', function Cal(){
-//    g.preventDefault();
-//})
-function Cal(){
-    let gender = document.getElementById("gender").value;
-    let age1 = document.getElementById("age").value;
-    let weight1 = document.getElementById("weight").value;
-    
-    let result = parseFloat(age1)+parseFloat(weight1);
-    if(!isNaN(result)){
-      document.getElementById("answer").innerHTML="You need this much protein in grams: "+ result;
-    }
-    //if(age == 0 || weight == 0 || gender == "")
-    //alert("Please enter values");
-    
-    //if(gender == "Male" || age <= 18 || weight <=130){
-    //    console.log("200 grams");
-    //}
-    
-    //document.getElementById("calculate").onclick = value;
+document.addEventListener('DOMContentLoaded', function (e) {
+  document.querySelector("#output-sec").classList.add("hide")
+  document.addEventListener('reset', function (e){
+  //  e.preventDefault();
+  document.querySelector("#gender").innerHTML ="";
+  document.querySelector("#age-input").innerHTML = "";
+  document.querySelector("#weight-input").innerHTML = "";
+  displayOutput("", "", "");
+
+  })
+  document.addEventListener('submit', function (e) {
+   e.preventDefault();
+   let gender = document.querySelector("#gender").value ;
+   document.querySelector("#gender-output").innerHTML = gender;
+   let age = document.querySelector("#age-input").value ;
+   document.querySelector("#age-output").innerHTML = age;
+   let weight = document.querySelector("#weight-input").value ;
+   document.querySelector("#weight-output").innerHTML = weight;
+   displayOutput(gender, age, weight);
+  })
+})
+function displayOutput(gender,age, weight){
+  if (showOutPut(gender,age,weight)){
+    document.querySelector("#output-sec").classList.remove("hide")
+    document.querySelector("#output-sec").classList.add("show")
+  }else{
+   document.querySelector("#output-sec").classList.add("hide")
+   document.querySelector("#output-sec").classList.remove("show")
+  }
 }
-//})
+function showOutPut(gender,age,weight){
+  if(gender.length >0 || age.length > 0 || weight.length > 0){
+    return true;
+  }else{
+    return false;
+  }
+}
